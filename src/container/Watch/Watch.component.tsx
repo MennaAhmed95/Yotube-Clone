9e9d0b7e563e9574c195ed0151c4aff0754c60de
MennaAhmed95 <mennamegahed95@github.com>
2023-01-29 16:19:20 +0200

    feature:add Watch Page

--- src/container/Watch/Watch.component.tsx
+++ src/container/Watch/Watch.component.tsx
@@ -1,3 +1,29 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { getVideoById } from "../../store/actions/videos.action";
+
 export const WatchPage = () => {
-  return <>WatchPage</>;
+  const { id } = useParams();
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getVideoById(id));
+  }, [dispatch, id]);
+
+  const { video } = useSelector((state: any) => state.selectedVideo);
+
+  return (
+    <>
+      <div>
+        <iframe
+          src={`https://www.youtube.com/embed/${id}`}
+          title={video?.snippet?.title}
+          allowFullScreen
+          width="100%"
+          height="500px"
+        ></iframe>
+      </div>
+    </>
+  );
 };
